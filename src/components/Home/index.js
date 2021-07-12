d3af8570ee103f14952e10c0ce24216794fb1d2a
James Canlas <jamescanlasjr@yahoo.com>
2021-07-11 19:18:33 -0700

    initial commit/start

---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+
+  return (
+    <section className="my-5" >
+      <h1 id="Home">Algorithms</h1>
+      <div className="my-2">
+      <p>
+      A site to hold some Algorithms and examples
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default Home
